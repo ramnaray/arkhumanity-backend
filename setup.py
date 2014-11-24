2ad771010fcc62ccbc679beff3ae651ebaa90928
ramnaray <rp01073@georgiasouthern.edu>
2014-11-23 20:56:12 -0700

    added setup.py with my config

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from setuptools import Command, setup
+
+
+setup(
+    name='arkhumanity-backend',
+    version='0.0.1-aplha',
+    url='http://github.com/ramnaray/arkhumanity-backend/',
+    license='Apache',
+    author='ramnaray',
+)
